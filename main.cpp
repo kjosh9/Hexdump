4c6ce820dbe2cc960ac58ea836a05dd9505791c2
J. Knestaut <kjosh9@vt.edu>
2017-01-18 23:36:55 -0500

    basic reading from file is implemented, prints contents to terminal

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
 
+
 #include <fstream>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
 
 int main(int argc, char* argv[]){
 
 
+	std::string filename = argv[1];
 
+	std::fstream theFile(argv[1]);
+	
+	std::string line;
 
-
+	if(theFile.is_open()){
+		
+		while(!theFile.eof()){
+			getline(theFile, line);
+			std::cout << line << std::endl;
+		}
+	}
+	else{
+		std::cout << "file does not exist" << std::endl;
+	}
+	
+	
+	
+	theFile.close();
+	
 
 	return 0;
 }

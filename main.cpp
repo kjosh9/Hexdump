fae6c6c34497b819b4bb92bfe8a9a380faf21a06
J. Knestaut <kjosh9@vt.edu>
2017-01-20 23:02:07 -0500

    fixed formating issue with address

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstdint>
+#include <iomanip>
 
 int main(int argc, char* argv[]){
 
@@ -12,20 +13,31 @@ int main(int argc, char* argv[]){
 	//get the file from the arguments and 
 	//open the file
 	std::string filename = argv[1];
-	std::fstream theFile(argv[1]);
+	std::ifstream inFile(argv[1], std::ios::binary);
 
-	std::string line;
-	int16_t linecount = 0;
+	int16_t address = 0;
+
+	uint16_t value;
+
+	//set the output base 
+	std::cout << std::hex;
 
 	//check if the file exist
-	if(theFile.is_open()){
+	if(inFile.is_open()){
 
 		//then get the contents of the file and 
 		//print line by line
-		while(!theFile.eof()){
-			getline(theFile, line);
-			std::cout << linecount << "\t" << line << std::endl;
-			linecount++;
+		while(!inFile.eof() && address < 200){
+
+			//print address with padding
+			std::cout << std::setw(7) << std::setfill('0') << address << ": ";
+
+			//inFile.read(reinterpret_cast<std::fstream::char_type*>(&value), sizeof value);
+			//std::cout << value << std::endl;
+
+			std::cout << std::endl;
+
+			address+=16;
 		}
 	}
 	else{
@@ -33,8 +45,7 @@ int main(int argc, char* argv[]){
 	}
 	
 
-
-	theFile.close();
+	inFile.close();
 
 	return 0;
 }
